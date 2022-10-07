f8b763982a70316aedac618a4cd201aa784f80cb
100451272 <100451272@alumnos.uc3m.es>
2022-10-07 17:29:04 +0200

    comenzado cookies register

---
+++ script/Register.js
@@ -0,0 +1,19 @@
+
+function validar() {
+    let email = document.forms["Reg"]["email"].value;
+    let username = document.forms["Reg"]["username"].value;
+    let password = document.forms["Reg"]["password"].value;
+    let repeat = document.forms["Reg"]["repeat"].value;
+    comp = checkPass(password, repeat);
+    const d = new Date();
+    d.setTime(d.getTime() + (2 * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = username + "=" + password + ";" + expires + ";path=/";
+  }
+
+function checkPass(pass, repeat) {
+    if (pass == repeat){
+        return true;
+    }
+    return false;
+}
