81e4b16ddc9ca8569605fb3a1c375f5eacafb700
ElCabote <alex.vargas.lop@gmail.com>
2022-10-17 18:01:14 +0200

    mazo cambios

--- script/Register.js
+++ script/Register.js
@@ -1,14 +1,14 @@
 
-function validar() {
+function validar(usuario, contrasena, expira) {
     let email = document.forms["Reg"]["email"].value;
-    let username = document.forms["Reg"]["username"].value;
-    let password = document.forms["Reg"]["password"].value;
+    usuario = document.forms["Reg"]["username"].value;
+    password = document.forms["Reg"]["password"].value;
     let repeat = document.forms["Reg"]["repeat"].value;
     comp = checkPass(password, repeat);
     const d = new Date();
     d.setTime(d.getTime() + (2 * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = username + "=" + password + ";" + expires + ";path=/";
+    expira = "expires="+d.toUTCString();
+    document.cookie = usuario + "=" + contrasena + ";" + expira + ";path=/";
   }
 
 function checkPass(pass, repeat) {
@@ -21,4 +21,17 @@ function checkPass(pass, repeat) {
 function checkCookie(){
     let user = getCookie("username");
     alert(user);
+}
+
+function getCookie(username){
+    let nombre = username + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    //to handle cookies with special characters, e.g. '$'
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i<ca.length; i++){
+        let c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+    }
 }
