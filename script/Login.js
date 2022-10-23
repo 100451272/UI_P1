ce7dbb77339fd03a427926cd5cab30636ee54904
ElCabote <alex.vargas.lop@gmail.com>
2022-10-23 20:08:25 +0200

    bckup

--- script/Login.js
+++ script/Login.js
@@ -1,5 +1,8 @@
-function submit(){
-    alert("Bienvenido");
+function check(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    console.log(username)
+    console.log(password)
 }
 
 
